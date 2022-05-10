c81e01963fc8dfd044274113d14833875bbda026
WE-DA-pluggg <79959576+WE-DA-pluggg@users.noreply.github.com>
2022-05-10 13:25:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,10 @@ from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.enrwf.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db = client.hotel
-
+import certifi
+ca = certifi.where()
+client = MongoClient('mongodb+srv://test:sparta@cluster0.enrwf.mongodb.net/Cluster0?retryWrites=true&w=majority',tlsCAFile=ca)
+db = client.dbsparta
 SECRET_KEY = 'sparta'
 
 @app.route('/')
