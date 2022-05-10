559454f07eda6f01d1c629bc6d6d3b32962d6b61
eunchong2lee <tlfqjrjs1234@naver.com>
2022-05-10 18:33:59 +0800

    Revert "커밋"
    This reverts commit 939fb12d5db3b9b6cb736d4524204cc5e17cd131.

--- app.py
+++ app.py
@@ -19,20 +19,7 @@ SECRET_KEY = 'sparta'
 
 @app.route('/')
 def home():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.users.find_one({"user_id": payload["user_id"]})
-        return redirect(url_for("info"))
-    except jwt.ExpiredSignatureError:
-        return redirect(url_for("login"))
-    except jwt.exceptions.DecodeError:
-        return redirect(url_for("login"))
-
-@app.route('/login')
-def login():
-    return render_template("login.html")
-
+        return render_template('login.html')
 
 
 @app.route('/sign_in', methods=['POST'])
@@ -92,12 +79,9 @@ def check_nickname_dup():
 @app.route('/main')
 def info():
     token_receive = request.cookies.get('mytoken')
-    try:
-        print("main token_recieved??: ", token_receive)
-        hotel_list = list(db.hotel.find({}, {'_id': False}))
-        return render_template('main.html', rows=hotel_list)
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("login"))
+    print("main token_recieved??: ", token_receive)
+    hotel_list = list(db.hotel.find({}, {'_id': False}))
+    return render_template('main.html', rows=hotel_list)
 
 @app.route("/info", methods=["POST"])
 def hotel_post():
